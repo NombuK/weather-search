1dee2a9eb3953189ce21821c74f2bee59bc39783
Nombulelo <nombulelokhumalo48@gmail.com>
2024-09-10 05:56:03 +0200

    Injected Html from Js (forecast)

--- src/index.js
+++ src/index.js
@@ -65,3 +65,28 @@ let searchFormEl = document.querySelector("#search-form");
 searchFormEl.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Norkem Park");
+
+function displayForecast() {
+  let days = ["Mon", "Tue", "Wed", "Thu", "Fri"];
+  let forecastHtml = " ";
+
+  days.forEach(function (day) {
+    forecastHtml += `
+            <div class="weather-forecast-day">
+              <div class="weather-forecast-date">${day}</div>
+              <div class="weather-forecast-icon">⛅</div>
+              <div class="weather-forecast-temperatures">
+                <div class="weather-forecast-temperature">
+                  <strong>15°C</strong>
+                </div>
+                <div class="weather-forecast-temperature">
+                  <strong>9°C</strong>
+                </div>
+              </div>
+            </div>`;
+  });
+  let forecastEl = document.querySelector("#forecast");
+  forecastEl.innerHTML = forecastHtml;
+}
+
+displayForecast();
